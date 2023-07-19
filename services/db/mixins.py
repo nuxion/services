0af316f47b46dd2f3e12767156fd5b9c527d7ddf
nuxion <nuxion@gmail.com>
2023-07-18 21:27:58 -0300

    BaseMixin mapped as DataClass

--- services/db/mixins.py
+++ services/db/mixins.py
@@ -1,7 +1,7 @@
 import datetime
 
-from sqlalchemy import DateTime
-from sqlalchemy.orm import Mapped, declarative_mixin, mapped_column
+from sqlalchemy import DateTime, BigInteger
+from sqlalchemy.orm import Mapped, declarative_mixin, mapped_column, MappedAsDataclass
 from sqlalchemy.sql import functions
 
 # from sqlalchemy.types import BINARY, DECIMAL, JSON, BigInteger
@@ -16,6 +16,34 @@ class DateTrackMixin:
     )
     updated_at: Mapped[datetime.datetime] = mapped_column(
         DateTime(),
+        server_default=functions.now(),
+        nullable=False,
+    )
+
+
+@declarative_mixin
+class DateTrackMixinAsDataclass(MappedAsDataclass):
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(), server_default=functions.now(), nullable=False, init=False
+    )
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(), server_default=functions.now(), nullable=False, init=False
+    )
+
+
+@declarative_mixin
+class BaseMixinAsDataclass(MappedAsDataclass):
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, init=False)
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(),
+        index=True,
+        init=False,
+        server_default=functions.now(),
+        nullable=False,
+    )
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(),
+        init=False,
         server_default=functions.now(),
         nullable=False,
     )
